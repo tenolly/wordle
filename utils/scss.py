1adace1886b4fb7db233591ba3be68bb4e791e73
tenolly <np50022005@yandex.ru>
2024-01-20 20:26:02 +0300

    add loggining

--- utils/scss.py
+++ utils/scss.py
@@ -3,4 +3,6 @@ import os
 
 for filename in os.listdir("app/static/scss"):
     if not filename.startswith("_"):
-        os.system(f"sass --no-source-map app/static/scss/{filename} app/static/css/{filename.replace('.scss', '.css')}")
+        source, target = "app/static/scss/" + filename, "app/static/css/" + filename.replace('.scss', '.css')
+        os.system(f"sass --no-source-map {source} {target}")
+        print(source, "->", target)
