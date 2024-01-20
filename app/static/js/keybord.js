2fe0c61aa73c11f168d5baf6c474b792b66173fd
tenolly <np50022005@yandex.ru>
2024-01-20 17:40:45 +0300

    splitting a function into several

--- app/static/js/keybord.js
+++ app/static/js/keybord.js
@@ -2,11 +2,17 @@ let currentLine = 0;
 let currentCell = 0;
 
 function pressKeybordButton(value) {
-    if (value.length == 1) {
-        if (currentCell < 5) {
-            let cell = document.getElementById(`line_${currentLine}_cell_${currentCell}`);
-            cell.innerHTML = value;
-            ++currentCell;
-        }
+    if (currentCell < 5) {
+        let cell = document.getElementById(`line_${currentLine}_cell_${currentCell}`);
+        cell.innerHTML = value;
+        ++currentCell;
     }
+}
+
+async function pressEnterButton() {
+
+}
+
+function pressBackspaceButton() {
+
 }
