cbeed00444e6d2a041a2bd32cde23b2d986ca9d2
tenolly <np50022005@yandex.ru>
2024-01-20 17:25:59 +0300

    update keyboard functionality

--- app/static/js/keybord.js
+++ app/static/js/keybord.js
@@ -1,3 +1,12 @@
+let currentLine = 0;
+let currentCell = 0;
+
 function pressKeybordButton(value) {
-    console.log(value);
+    if (value.length == 1) {
+        if (currentCell < 5) {
+            let cell = document.getElementById(`line_${currentLine}_cell_${currentCell}`);
+            cell.innerHTML = value;
+            ++currentCell;
+        }
+    }
 }
