7c649cdbf578d1bf86bae61358d311f595ce286c
tenolly <np50022005@yandex.ru>
2024-01-22 00:37:03 +0300

    update make_dict util

--- utils/make_dict.py
+++ utils/make_dict.py
@@ -1,14 +1,33 @@
-from string import ascii_lowercase as letters
+# Guide how to add your language (for windows, the linux manual find yourself)
+# https://stackoverflow.com/questions/65267036/add-libreoffice-dictionary-to-pyenchant-on-windows-for-anaconda
+
+import datetime
 from itertools import product
 
+from colorama import Fore
+import argparse
 import enchant
 
+parser = argparse.ArgumentParser()
+parser.add_argument("language", help="the dictionary will be created for that language")
+parser.add_argument("alphabet", help="all the letters of the alphabet, case insensitive")
+parser.add_argument("target_file")
 
-d = enchant.Dict("en_US")
+args = parser.parse_args()
+if not enchant.dict_exists(args.language):
+    raise ValueError("dictionary not installed")
+
+d = enchant.Dict(args.language)
+
+print(f"run from {Fore.GREEN + datetime.datetime.now().strftime('%H:%M:%S') + Fore.RESET} to ", end="")
 
 words = []
-for word in filter(d.check, map("".join, product(letters, repeat=5))):
+for word in filter(d.check, map("".join, product(args.alphabet.lower(), repeat=5))):
     words.append(word)
 
-with open("data/en_dict.txt", mode="w") as file:
+print(Fore.GREEN + datetime.datetime.now().strftime('%H:%M:%S') + Fore.RESET, end="  ==>  ")
+
+with open(args.target_file, mode="w") as file:
     file.write("\n".join(words))
+
+print(f"{Fore.GREEN + str(len(words)) + Fore.RESET} words saved to {args.target_file}")
