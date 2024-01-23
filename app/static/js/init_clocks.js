08beb9ff6427444f3f830bf26b0f7577fb3b62a5
tenolly <np50022005@yandex.ru>
2024-01-23 23:09:54 +0300

    delete seconds hand

--- app/static/js/init_clocks.js
+++ app/static/js/init_clocks.js
@@ -1,5 +1,4 @@
 fetch("/get_server_time").then((response) => response.json()).then((json) => {
     document.getElementById("clock__hours").style.transform = `rotate(${json.hour * 30 + json.minute / 2}deg)`;
     document.getElementById("clock__minutes").style.transform = `rotate(${json.minute * 6}deg)`;
-    document.getElementById("clock__seconds").style.transform = `rotate(${json.second * 6}deg)`;
 })
